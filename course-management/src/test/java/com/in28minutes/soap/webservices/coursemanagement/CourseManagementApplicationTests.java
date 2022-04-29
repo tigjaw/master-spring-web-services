d9c4e992217e5f8f0e859a23fb6bc124eed3a0ff
Joshua <tigjaw@gmail.com>
2022-04-29 19:22:07 +0100

    37.05

---
+++ course-management/src/test/java/com/in28minutes/soap/webservices/coursemanagement/CourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.soap.webservices.coursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
